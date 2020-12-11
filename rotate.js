ead980feb969e4b61c9ff3971e5d277da4ff9d95
Fernando Giroto <fernandogiroto@iMacdeFernando.home>
2020-12-11 13:06:12 +0000

    first_commit

---
+++ rotate.js
@@ -0,0 +1,12 @@
+function change_text_passo1(){
+    document.getElementById("text").innerHTML = "Novo texto virtopia, etapa 01 do processo de criação";;
+}
+
+function change_text_passo2(){
+    document.getElementById("text").innerHTML = "Novo texto virtopia, etapa 02 do processo de criação";
+}
+
+function change_text_passo3(){
+    document.getElementById("text").innerHTML = "Novo texto virtopia, etapa 03 do processo de criação";
+}
+
