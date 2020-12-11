72d5809ab6dc710de5e6b3d89c17173a7b23942a
Fernando Giroto <fernandogiroto@iMacdeFernando.home>
2020-12-11 13:16:15 +0000

    update

--- rotate.js
+++ rotate.js
@@ -1,12 +1,20 @@
 function change_text_passo1(){
-    document.getElementById("text").innerHTML = "Novo texto virtopia, etapa 01 do processo de criação";;
+    document.getElementById("text_title").innerHTML = "AR: Realidade Aumentada";
+    document.getElementById("text").innerHTML = "Vizualização do mundo real com a sobreposição de elementos digitais.";
 }
 
 function change_text_passo2(){
-    document.getElementById("text").innerHTML = "Novo texto virtopia, etapa 02 do processo de criação";
+   document.getElementById("text_title").innerHTML = "VR: Realidade Virtual";
+    document.getElementById("text").innerHTML = "Imersão em um mundo totalmente digital, onde o usuário se sente presente através de diversas interações.";
 }
 
 function change_text_passo3(){
-    document.getElementById("text").innerHTML = "Novo texto virtopia, etapa 03 do processo de criação";
+    document.getElementById("text_title").innerHTML = "MR: Realidade Mista";
+    document.getElementById("text").innerHTML = "Sincronização de um ambiente totalmente digital, com o espaço físico real.";
 }
 
+function change_text_passo4(){
+    document.getElementById("text_title").innerHTML = "XR: Realidade Estendida";
+    document.getElementById("text").innerHTML = "Abrange todas as formas de imersão já conhecidas.";
+
+}
